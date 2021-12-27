9513a561ffe70ea2fcb8765a8d97bcda32e56c9b
fazaamal <fazasophian123@gmail.com>
2021-12-27 12:51:51 +0800

    First commit

---
+++ src/Car.java
@@ -0,0 +1,77 @@
+public class Car {
+    private String make, model, type, colour;
+    private int year, seats;
+    private double costPerDay, tyreSize, weight;
+
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public double getCostPerDay() {
+        return costPerDay;
+    }
+
+    public double getTyreSize() {
+        return tyreSize;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public void setCostPerDay(double costPerDay) {
+        this.costPerDay = costPerDay;
+    }
+
+    public void setTyreSize(double tyreSize) {
+        this.tyreSize = tyreSize;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+}
